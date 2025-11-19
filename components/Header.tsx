bea0525763e196ef87d54ab54b88f6f68ef86bae
EvgenyShil <61139777+EvgenyShil@users.noreply.github.com>
2025-11-19 15:53:07 +0300

    Hide unfinished nav links

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,10 +6,6 @@ export default function Header(){
       <div className="container h-16 flex items-center justify-between">
         <Link href="/" className="font-semibold text-lg">3D Studio</Link>
         <nav className="flex gap-4 text-sm">
-          <Link href="/3d-print" className="hover:underline">3D-печать</Link>
-          <Link href="/3d-modeling" className="hover:underline">3D-моделирование</Link>
-          <Link href="/models" className="hover:underline">Модели</Link>
-          <Link href="/cases" className="hover:underline">Кейсы</Link>
           <Link href="/prices" className="hover:underline">Цены</Link>
           <Link href="/faq" className="hover:underline">FAQ</Link>
           <Link href="/contacts" className="hover:underline">Контакты</Link>
